f5d5503209ac8bd9a21d5d8db0e3abeb6c47639b
Jonah Snider <me@jonahsnider.ninja>
2019-09-06 21:24:24 -0700

    fix: fix how document.getElementById is used
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- src/assets/ts/constants.ts
+++ src/assets/ts/constants.ts
@@ -9,27 +9,27 @@ export const apiURL = "https://us-central1-zero-width-shortener.cloudfunctions.n
 export const hostnames = ["zws.im", "zws.jonahsnider.ninja", "zerowidthshortener.netlify.com"];
 
 // We do this to have avoid having "document.getElementById" a lot, since Parcel will rename this variable to something short
-const getElementById = document.getElementById;
+const doc = document;
 
 /**
  * HTML elements used across the project.
  */
 export const elements = {
   submitButtons: {
-    shorten: getElementById("shorten-submit") as HTMLButtonElement,
-    stats: getElementById("stats-submit") as HTMLButtonElement
+    shorten: doc.getElementById("shorten-submit") as HTMLButtonElement,
+    stats: doc.getElementById("stats-submit") as HTMLButtonElement
   },
   inputs: {
-    shorten: getElementById("shorten-url") as HTMLInputElement,
-    stats: getElementById("stats-url") as HTMLInputElement
+    shorten: doc.getElementById("shorten-url") as HTMLInputElement,
+    stats: doc.getElementById("stats-url") as HTMLInputElement
   },
   outputs: {
-    shorten: getElementById("shorten-result") as HTMLInputElement,
-    stats: getElementById("stats-result") as HTMLParagraphElement
+    shorten: doc.getElementById("shorten-result") as HTMLInputElement,
+    stats: doc.getElementById("stats-result") as HTMLParagraphElement
   },
   forms: {
-    shorten: getElementById("shorten") as HTMLFormElement,
-    stats: getElementById("stats") as HTMLFormElement
+    shorten: doc.getElementById("shorten") as HTMLFormElement,
+    stats: doc.getElementById("stats") as HTMLFormElement
   },
-  copyButton: getElementById("copy") as HTMLButtonElement
+  copyButton: doc.getElementById("copy") as HTMLButtonElement
 };
